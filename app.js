3b4832c48750a9b1ad381518f9021f999f286bca
DOng <khanhdong1702pro@gmail.com>
2024-05-01 18:25:21 +0700

    update tinh nang dang ki email

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
-
+const bodyParser = require('body-parser');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.use('/admin', indexRouter);
 app.use('/', usersRouter);
