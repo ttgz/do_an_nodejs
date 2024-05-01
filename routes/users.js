dbec9b701ac76cef3f9ef36d94b929aef039c334
DOng <khanhdong1702pro@gmail.com>
2024-05-02 02:49:29 +0700

    update tinh nang

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,43 @@ const con = mysql.createConnection({
 });
 
 con.connect()
+//xử lý req vào trang chủ
 router.get('/', function (req, res, next) {
   let sql = 'select * from thongtinlienhe'
-  con.query(sql, (err, footer) => {
-    res.render('users/trangchu', { footer: footer });
+  let listBaiVietMoiNhat, listBaiVietXemNhieuNhat 
+
+  //3 bài viết mới nhất
+  con.query('select * from baiviet order by ngay_dang desc limit 3', (err,result)=>{
+    listBaiVietMoiNhat = result
+    //3 bài viết xem nhiều nhất
+    con.query('select * from baiviet order by luot_xem desc limit 3',(err,resultDSBV)=>{
+      listBaiVietXemNhieuNhat = resultDSBV
+      con.query('select ten_danh_muc from danhmucbaiviet',(err,resultDanhMucBaiViet)=>{
+          con.query('select ten_danh_muc, baiviet.* from danhmucbaiviet inner join baiviet on danhmucbaiviet.id = baiviet.danh_muc',(err,resultBaiViet)=>{
+            con.query('select * from thongtinlienhe',(err,resultTTLH)=>{
+              res.render('users/trangchu',{moinhat: listBaiVietMoiNhat, xemnhieunhat: listBaiVietXemNhieuNhat, danhmuc: resultDanhMucBaiViet, danhSachBaiViet:resultBaiViet,footer:resultTTLH})
+            })
+          })
+      })
+    })
   })
+ 
+  
 });
 
+//xử lý tăng lượt xem của bài viết
+router.get('/chitietbaiviet/:id',(req,res)=>{
+  let id = req.params.id
+  let sql = `select * from baiviet where id = ${id}`
+  con.query(sql,(err,result)=>{
+      let luotXem = result[0].luot_xem + 1
+      sql = `update baiviet set luot_xem = ${luotXem} where id = ${id}`
+      con.query(sql,(err,resultUpdate)=>{
+        res.redirect('/')
+      })
+  })
+})
+// xử lý req người dùng đăng kí
 router.post('/dangki', (req, res) => {
   emailDangKi = req.body.emailDangKi
   let sql = `select id from dangky where email = '${emailDangKi}'`
