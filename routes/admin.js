74d6ff7b2921d53f714976241e3b9382786a85ba
unknown <nguyenngochoang192k4@gmail.com>
2024-05-02 09:05:06 +0700

    layouts_admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('admin/layouts', { content: 'main.ejs' });
 });
-
+router.get('/manageruser', function(req, res, next) {
+  res.render('admin/layouts', { content: 'manager_user.ejs' });
+});
+router.get('/managerpost', function(req, res, next) {
+  res.render('admin/layouts', { content: 'manager_post.ejs' });
+});
+router.get('/managertopicpost', function(req, res, next) {
+  res.render('admin/layouts', { content: 'manager_topic_post.ejs' });
+});
 module.exports = router;
